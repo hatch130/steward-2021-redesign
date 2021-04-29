c3b0e40475e7bed5e0a1eb9ddfeae8c2da2f16d4
SiteNineStudios <dev@siteninestudios.com>
2021-04-29 15:50:37 +0500

    Services Pages Sprint 3

--- docs/all.js
+++ docs/all.js
@@ -1,13 +1,12 @@
 var app = {};
 app.global = {
     init: function () {
-        console.log("load global functions"),
-            app.global.loadHeader(),
-            app.global.header(),
-            app.global.hero(),
-            app.global.dialog(),
-            app.global.news(),
-            app.global.services();
+        app.global.loadHeader();
+        app.global.header();
+        app.global.hero();
+        app.global.dialog();
+        app.global.news();
+        app.global.services();
 
         app.global.fixedHeader();
 
@@ -15,20 +14,20 @@ app.global = {
     header: function () {
         $(".alert-box .dismiss").click(function () {
             $(this).closest(".alert-box").hide()
-        }),
+        });
         $(".header .menu-icon").click(function () {
             $(this).toggleClass("open"),
                 $(".header, body, .header .menu").toggleClass("open")
-        }),
+        });
         $(".header .language label").click(function () {
             $(this).closest(".language").toggleClass("open")
-        }),
+        });
         $(".header .search-icon, .header .close").click(function () {
             $(".header .search-wrapper, .header .search-input, .header .search-icon").toggleClass("open")
-        }),
+        });
         $(".menu-items .item > a").click(function () {
             $(this).next().hasClass("sub") && $(this).parent().toggleClass("open")
-        })
+        });
     },
     hero: function () {
         $(".hero .toggle span").click(function () {
@@ -54,7 +53,7 @@ app.global = {
             var id = $(this).attr("href");
 
             $('html, body').animate({
-                scrollTop: $(id).offset().top - 50
+                scrollTop: $(id).offset().top - 230
             }, 500);
         });
 
@@ -64,6 +63,40 @@ app.global = {
             $(this).closest(".accordian").toggleClass("active");
 
         });
+
+
+        $('.module-accordian .accordian .title').click(function(){
+            $('.accordian.active').not($(this).closest('.accordian')).removeClass('active').find('.content').slideUp();
+            $(this).closest('.accordian').toggleClass('active').find('.content').slideToggle();
+        });
+
+        $(".module-awards.slider > div").slick({
+            variableWidth: true,
+            dots: true,
+            arrows: false,
+            slidesToShow: 5,
+            infinite: false 
+        });
+
+        $(".module-video .video-list").slick({
+            infinite: false,
+            variableWidth: true,
+            nextArrow: '<button type="button" class="slick-next"><i class="icon-right-bold"></i></button>',
+            prevArrow: '<button type="button" class="slick-prev"><i class="icon-right-bold"></i></button>'
+        });
+
+
+        $(".module-testimonials .list").slick({
+            infinite: false,
+            nextArrow: '<button type="button" class="slick-next"><i class="icon-right"></i></button>',
+            prevArrow: '<button type="button" class="slick-prev"><i class="icon-right"></i></button>'
+        });
+
+        $(".page-team .team-members .more").click(function(e){
+            e.preventDefault();
+            $(this).closest('.item').find('.text').toggleClass("open");
+        })
+
     },
     dialog: function () {
         $(".dialog").click(function (e) {
